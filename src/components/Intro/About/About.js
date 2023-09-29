1982c06d46dcba2d3074da12e651ed3c5e3ccc06
youth-story <140192100+youth-story@users.noreply.github.com>
2023-09-30 00:00:57 +0530

    Add files via upload

--- src/components/Intro/About/About.js
+++ src/components/Intro/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Input from './Input';
 import styles from './About.module.css';
+import mob_screen from './mob_screen.png';
 
 export default function About({email, validated, setIsValidated, handleEmailChange, disabled, setDisabled}) {
 
@@ -11,6 +12,9 @@ export default function About({email, validated, setIsValidated, handleEmailChan
             <h2 className={styles.heading2}>FOR OUR FIRST USERS</h2>
             <p>We'll launch all in one platform with a huge number of features that you will definitely like.</p>
             <p>And now you can become the very first user and get a god mode absolutely free.</p>
+            <div className={styles.imgContainer}>
+                <img className={styles.img} src={mob_screen} alt='Image' />
+            </div>
             <Input validated={validated} setIsValidated={setIsValidated} email={email} handleEmailChange={handleEmailChange} disabled={disabled} setDisabled={setDisabled} />
         </div>
         </>
