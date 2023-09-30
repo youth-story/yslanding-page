6399389b54942e9068a4d5b806618c4d3e3f9535
youth-story <140192100+youth-story@users.noreply.github.com>
2023-09-30 21:56:55 +0530

    Update LandingPage.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,70 +1,68 @@
-import React, { useState, useRef, useEffect } from 'react';
-import Intro from '../components/Intro/Intro';
-import Info from '../components/Info/Info';
-import Footer from '../components/Footer/Footer';
-import styles from './LandingPage.module.css';
-import { collection, addDoc } from "firebase/firestore";
-import { db } from './firebase';
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/D2DLogo.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="🚀 Exciting News: Our Waiting List Page is Live! Get 6 Months FREE - Register Now! 🚀
+      
+      what's coming for you :
+      
+      - 💡 Interviews with budding innovators and artists.
+      -  🧑 Profile  Details 
+      - 👗 Features on fashion trends and styles.
+      - 🎬 Reviews on the latest movies, games, and tech.
+      - 🌍 Travel guides and destination recommendations.
+      - 🌿 Health and wellness tips and advice.
+      - 💬 Opinion pieces and essays on social issues.
+      - 🎓 Career and education advice.
+      
+      We've got it all tailored to your interests and concerns!
+      
+      Ready to Dive In? Here's How:
+      1️⃣ Visit our website: 
+      www.youthstory.in
+      2️⃣ Join our Waiting List - it's live now!
+      3️⃣ Be among the FIRST users and enjoy 6 months for FREE!
+      4️⃣ Hurry, secure your spot today! Don't miss out on this opportunity to connect, inspire, and grow with YOUTH STORY.
+      
+      
+      🌟 Be among the FIRST to join our community, and we're giving you a FREE 6-month subscription! 🌟
+      Don't miss out! Register now and be part of something incredible. Let's make YOUTH STORY your story. 🌟
+      Contact: d2d@youthstory.in"
+    />
+    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+    <!--
+      manifest.json provides metadata used when your web app is installed on a
+      user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
+    -->
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+    <!--
+      Notice the use of %PUBLIC_URL% in the tags above.
+      It will be replaced with the URL of the `public` folder during the build.
+      Only files inside the `public` folder can be referenced from the HTML.
 
-export default function LandingPage() {
-    const scroll = useRef(null);
-    const [email, setEmail] = useState('');
-    const [disabled, setDisabled] = useState(false);
-    const [validated, setIsValidated] = useState(false);
-    const [googleMapsLink, setGoogleMapsLink] = useState('');
-    const [userLocation, setUserLocation] = useState({});
+      Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
+      work correctly both with client-side routing and a non-root public URL.
+      Learn how to configure a non-root public URL by running `npm run build`.
+    -->
+    <title>Youth Story | D2D - Dare 2 Dream</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+    <!--
+      This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
 
-    const handleEmailChange = (value) => {
-        setEmail(value);
-    };
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the <body> tag.
 
-    useEffect(() => {
-        // Get the user's location here using Geolocation API
-        if (validated && email !== 'Successfully Registered!' && "geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                const latitude = position.coords.latitude;
-                const longitude = position.coords.longitude;
-
-                // Now you have the user's coordinates
-                setUserLocation({ latitude, longitude });
-
-                // Create a Google Maps link with the stored coordinates
-                if (latitude && longitude) {
-                    const mapsLink = `https://www.google.com/maps?q=${latitude},${longitude}`;
-                    setGoogleMapsLink(mapsLink);
-
-                    // Perform the asynchronous operation (adding a document to Firestore) with coordinates and the Google Maps link
-                    addDoc(collection(db, "d2d"), {
-                        email: email,
-                        coordinates: { latitude, longitude },
-                        googleMapsLink: mapsLink,
-                    })
-                    .then((docRef) => {
-                        // Handle success (you can log or provide user feedback)
-                        // console.log("Email document added to Firestore:", docRef.id);
-                        handleEmailChange('Successfully Registered!');
-                    })
-                    .catch((error) => {
-                        // Handle errors gracefully, provide feedback to the user, and log for debugging
-                        // console.error("Error adding email document to Firestore:", error);
-                        alert("Error in registering, please try again");
-                    });
-                }
-            }, function (error) {
-                return;
-            });
-        }
-    }, [validated, email]);
-
-    const scrollToInfo = () => {
-        scroll.current.scrollIntoView({ behavior: 'smooth' });
-    };
-
-    return (
-        <div className={styles.container}>
-            <Intro email={email} scroller={scrollToInfo} handleEmailChange={handleEmailChange} disabled={disabled} setDisabled={setDisabled} validated={validated} setIsValidated={setIsValidated} />
-            <br /><br /><br /><div ref={scroll}><Info /></div>
-            <Footer email={email} handleEmailChange={handleEmailChange} disabled={disabled} setDisabled={setDisabled} validated={validated} setIsValidated={setIsValidated} />
-        </div>
-    );
-}
+      To begin the development, run `npm start` or `yarn start`.
+      To create a production bundle, use `npm run build` or `yarn build`.
+    -->
+  </body>
+</html>
